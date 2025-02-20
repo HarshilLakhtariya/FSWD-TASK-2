29deef14dfa9f625f90d03ca9cfa2a4ebf22a1b7
HarshilLakhtariya <161796914+HarshilLakhtariya@users.noreply.github.com>
2025-02-20 16:04:52 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,5 @@
+// Add interactivity if needed
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for contacting me! I will get back to you soon.');
+});
